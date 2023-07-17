8c8e3de7553c6f5ec584f4138d78d3970e8b5691
Francesc Perez <fperez@sklum.com>
2023-07-17 08:52:01 +0200

    fix: removed broadCastReceiver

--- android/src/main/kotlin/com/tlt/flutter_zebra_sdk/FlutterZebraSdkPlugin.kt
+++ android/src/main/kotlin/com/tlt/flutter_zebra_sdk/FlutterZebraSdkPlugin.kt
@@ -5,7 +5,6 @@ import android.bluetooth.BluetoothAdapter
 import android.bluetooth.BluetoothDevice
 import android.bluetooth.BluetoothHeadset
 import android.bluetooth.BluetoothSocket
-import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
 import android.content.IntentFilter
@@ -199,24 +198,6 @@ class FlutterZebraSdkPlugin : FlutterPlugin, MethodCallHandler, ActivityAware {
     } finally {
       // Close the connection to release resources.
       conn.close()
-    }
-  }
-
-  class BluetoothBroadcastReceiver: BroadcastReceiver() {
-    override fun onReceive(context: Context?, intent: Intent?) {
-      Log.d("BluetoothReceiver", "${intent?.action}")
-
-      when (intent?.action) {
-        BluetoothDevice.ACTION_ACL_CONNECTED -> {
-          val device: BluetoothDevice = intent?.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE)
-          Log.d("BluetoothReceiver", "BluetoothDevice ${device.name} connected")
-        }
-        BluetoothDevice.ACTION_ACL_DISCONNECTED -> {
-          val device: BluetoothDevice = intent?.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE)
-          Log.d("BluetoothReceiver", "BluetoothDevice ${device.name} disconnected")
-        }
-
-      }
     }
   }
 
